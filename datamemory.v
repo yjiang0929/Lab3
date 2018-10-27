cc7d2e38a83c68be98bf2b6cc58cc1edd217d206
Adam Selker <adamselker@gmail.com>
2018-10-26 20:10:59 -0400

    New data memory

--- datamemory.v
+++ datamemory.v
@@ -1,31 +1,23 @@
-//------------------------------------------------------------------------
-// Data Memory
-//   Positive edge triggered
-//   dataOut always has the value mem[address]
-//   If writeEnable is true, writes dataIn to mem[address]
-//------------------------------------------------------------------------
+// Data memory, with automatic initialization
+// Credit: Ben Hill
 
-module datamemory
-#(
-    parameter addresswidth  = 7,
-    parameter depth         = 2**addresswidth,
-    parameter width         = 32
-)
+module memory
 (
-    input 		                clk,
-    output reg [width-1:0]      dataOut,
-    input [addresswidth-1:0]    address,
-    input                       writeEnable,
-    input [width-1:0]           dataIn
+  input clk, regWE,
+  input[9:0] Addr,
+  input[31:0] DataIn,
+  output[31:0]  DataOut
 );
-
-
-    reg [width-1:0] memory [depth-1:0];
-
-    always @(posedge clk) begin
-        if(writeEnable)
-            memory[address] <= dataIn;
-        dataOut <= memory[address];
+  
+  reg [31:0] mem[1023:0];  
+  
+  always @(posedge clk) begin
+    if (regWE) begin
+      mem[Addr] <= DataIn;
     end
-
+  end
+  
+  initial $readmemh(“file.dat”, mem);
+    
+  assign DataOut = mem[Addr];
 endmodule
