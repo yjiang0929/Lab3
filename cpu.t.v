cae1fd3215901e4e2443892554f127e8682f9636
NickShermeister <ncsherman159@gmail.com>
2018-10-30 11:38:03 -0400

    cpu.t.v is having issues related to checking register values.

--- cpu.t.v
+++ cpu.t.v
@@ -1,5 +1,5 @@
-`timescale 1 ns / 1 ps
-include "cpu.v"
+// `timescale 1 ns / 1 ps
+`include "cpu.v"
 `define DELAY 5000
 
 module testCPU();
@@ -23,25 +23,45 @@ always #10 clk=!clk;
   initial begin
     $dumpfile("cpu.vcd");
     $dumpvars();
+    $display("Before begintest");
     begintest = 0;
+    $display("After begintest");
+    #1000;
+    $display("After delay");
     begintest = 1;
-    #1000000;
+    $display("After begintest = 1");
     $finish();
   end
 
 always @(posedge endtest) begin
   $display("DUT passed? %b", dutpassed);
-
 end
 
 endmodule
 
 module testbench
 (
-  input begintest,
+  input wire begintest,
   output reg endtest,
   output reg dutpassed,
 
   input wire clk
 
   );
+  wire[31:0] hanoi = 32'd270;
+  // $display("Testbench start");
+  always @(posedge begintest) begin
+    dutpassed <= 1;
+    $display("Entered always loop");
+    $display("%b", dut.rf.reg2.quot);
+    $display("%b", hanoi);
+    if(dut.rf.reg2.qout != hanoi) begin
+    $display("Test failed: Tower of Hanoi answer unexpected; expected %b but got %b", hanoi, dut.rf.reg2.qout);
+    dutpassed <= 0;
+    end
+
+  endtest = 1;
+  $display("Endtest: %b ", endtest);
+  end
+
+  endmodule
