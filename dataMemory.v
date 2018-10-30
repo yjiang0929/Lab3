9c032359dde1b956b06504dd2c7939afd265fdf5
NickShermeister <ncsherman159@gmail.com>
2018-10-29 21:37:14 -0400

    fixing up data memory for 2 read ports.

--- dataMemory.v
+++ dataMemory.v
@@ -5,19 +5,24 @@ module dataMemory
 (
   input clk, regWE,
   input[9:0] Addr,
+  input[9:0] CmdAddr,
   input[31:0] DataIn,
-  output[31:0]  DataOut
+  output[31:0]  DataOut,
+  output[31:0] CmdOut
 );
-  
-  reg [31:0] mem[1023:0];  
-  
+
+  reg [31:0] mem[1023:0];
+
+  //TODO: Divide by 4?
+
   always @(posedge clk) begin
     if (regWE) begin
       mem[Addr] <= DataIn;
     end
   end
-  
+
 	initial $readmemh("file.dat", mem);
-    
+
   assign DataOut = mem[Addr];
+  assign CmdOut = mem[CmdAddr];
 endmodule
