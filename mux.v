6075d2f388d9c989174ba058f8cf666d6e7b4f4e
Adam Selker <adamselker@gmail.com>
2018-10-30 22:09:53 -0400

    Simplify PC and fix mux3

--- mux.v
+++ mux.v
@@ -1,13 +1,13 @@
 module mux2 // 2 inputs of adjustable width, 1 address pin
 #(parameter width = 32)
 (
-	output [width-1:0] chosenOne, // Output
-	input [width-1:0] harry, // Inputs, with arbitrary depth
-	input [width-1:0] neville,
-	input dumbledore // Address
+	output [width-1:0] out, // Output
+	input [width-1:0] in1, // Inputs, with arbitrary depth
+	input [width-1:0] in2,
+	input select // Address
 );
 
-	assign chosenOne = (dumbledore ? neville : harry);
+	assign out = (select ? in2 : in1);
 
 endmodule
 
@@ -15,13 +15,13 @@ endmodule
 module mux3 // 3 inputs of adjustable width, 2 address pins
 #(parameter width = 32)
 (
-	output [width-1:0] chosenOne, // Output
-	input [width-1:0] harry, // Inputs, with arbitrary depth
-	input [width-1:0] neville,
-	input [width-1:0] draco,
-	input [1:0] dumbledore // Address
+	output [width-1:0] out, // Output
+	input [width-1:0] in1, // Inputs, with arbitrary depth
+	input [width-1:0] in2,
+	input [width-1:0] in3,
+	input [1:0] select // Address
 );
 
-	assign chosenOne = (dumbledore[0] ? (dumbledore[1] ? draco : neville) : harry);
+	assign out = (select[1] ? in3 : (select[0] ? in2 : in1));
 
 endmodule
