9f4d8870daabdcc89c70c777d2119ae7a6066f39
NickShermeister <ncsherman159@gmail.com>
2018-10-31 23:01:30 -0400

    We now have a scripted tests .sh in order to script run the cpu.t.v. Victory has been achieved.

--- cpu.t.v
+++ cpu.t.v
@@ -9,8 +9,12 @@ module testCPU();
   reg endtest = 0; //Set high to signal test completion
   reg dutpassed = 1; //Indicates whether register file passed tests
 
-  wire[31:0] hanoi = 32'd270;
-  wire[31:0] fib = 32'd58;
+  //Order of tests: ours (bleep_vim), dazedandconfused, jumpingfoxes, ninja, sree, storemoney
+  wire[31:0] hanoi = 32'd270; //0
+  // wire[31:0] array_loop = ; //7 //another array test; come back to
+  wire[31:0] fib = 32'd58; //8 $v0
+  wire[31:0] yeet = 32'd121; //11 t0
+
   wire[31:0] simple = 32'd4;
 
   //Instantiate dut
@@ -24,8 +28,6 @@ module testCPU();
   reg[1023:0] test_num;
   reg[1023:0] test_str;
 
-
-
 initial clk = 0;
 always #10 clk=!clk;
 
@@ -35,54 +37,50 @@ always #10 clk=!clk;
 	    $finish();
         end
 
-  if (! $value$plusargs("test_num=%s", test_num)) begin
+  if (! $value$plusargs("test_num=%d", test_num)) begin
     $display("ERROR: Test name not specified. Provide +test_num=[test_num] argument");
     $finish();
     end
 
-  test_str <= "fib_func";
-  r_RESULT <= 0;
-
-
-	// if (! $value$plusargs("dump_fn=%s", dump_fn)) begin
-	//     $display("ERROR: provide +dump_fn=[path for VCD dump] argument");
-	//     $finish();
-        // end
-
-
-    // $readmemh("asmtest/NINJA/fib_func/fib_func.text.hex", dut.dm.mem, 0);
     $readmemh(mem_text_fn, dut.dm.mem,0);
-    // $readmemh("asmtest/bleep_vim/branch_test.text.hex", dut.dm.mem, 0);
-
-
 
     $dumpfile("cpuout.vcd");
     $dumpvars();
 
     begintest = 1;
-    $display("Testbench start");
 
-    $display("After begintest = 1");
     #200000;
 
     dutpassed = 1;
 
 
-    $display("%b", test_num);
-    case (test_num)
-      3'd0  : r_RESULT <= 0;
-      3'b001  : r_RESULT <= 1;
-      3'b010  : r_RESULT <= 2;
-      default : r_RESULT <= 9;
-    endcase
-
-    $display("R_RESULT: %b", r_RESULT);
-    if(r_RESULT == 0) begin
-      if(dut.rf.reg2.qout != fib) begin
+    if(test_num == 0) begin
+    $display("got here");
+      if(dut.rf.reg2.qout != hanoi) begin
       $display("Test failed: Tower of Hanoi answer unexpected; expected %b but got %b", hanoi, dut.rf.reg2.qout);
       dutpassed = 0;
       end
     end
+    if(test_num == 1) begin
+    $display("NOT IMPLMENTED YET");
+      // if(dut.rf.reg2.qout != ) begin
+      // $display("Test failed: answer unexpected; expected %b but got %b", , dut.rf.reg2.qout);
+      // dutpassed = 0;
+      // end
+    end
+    if(test_num == 2) begin
+      if(dut.rf.reg2.qout != fib) begin
+      $display("Test failed: answer unexpected; expected %b but got %b", fib, dut.rf.reg2.qout);
+      dutpassed = 0;
+      end
+    end
+    if(test_num == 3) begin
+      if(dut.rf.reg2.qout != yeet) begin
+      $display("Test failed: answer unexpected; expected %b but got %b", yeet, dut.rf.reg2.qout);
+      dutpassed = 0;
+      end
+    end
+
 
     endtest = 1;
     $display("DUT passed? %b", dutpassed);
