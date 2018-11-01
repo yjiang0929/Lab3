1097c564f4ddf2965426e903a9a5c750f6ea4d42
Adam Selker <adamselker@gmail.com>
2018-10-31 21:29:01 -0400

    Comment cpu

--- cpu.v
+++ cpu.v
@@ -11,18 +11,15 @@ module cpu(
 	input clk
 );
 
-	reg [31:0] pc;
-
-	initial pc <=0;
-
-	always @(posedge clk) pc <= nextPc;
-	// always @(posedge clk) pc <= pc + 32'd4;
-
-
-	wire [31:0] nextPc, Da, Db, DbOrImm, Dw, resAluRes, immExt, memAddr, memOut, cmdOut, pcAluRes, pcAdd, branchAluRes;
+	wire [31:0] nextPc, Da, Db, DbOrImm, Dw, resAluRes, immExt, memAddr, memOut, cmd, pcAluRes, pcAdd, branchAluRes;
 	wire zeroFlag;
+	
+	reg [31:0] pc;  // Program counter is registers; the last 2 bits are ignored because we always run words
+	initial pc <=0; // Initialize at 0
+	always @(posedge clk) pc <= nextPc; // Update every clock cycle
 
-	// These should be set by the decoder
+
+	// These are driven by the decoder
 	wire immSel, regWrEn,  memWrEn;
 	wire [1:0] DwSel, pcSel, jSel;
 	wire [2:0] resAluOp;
@@ -30,37 +27,32 @@ module cpu(
 	wire [15:0] imm;
 	wire [25:0] jumpAddr;
 
+	// The decoder drives all of the control logic, based on the current instruction.
+	decoder dec(cmd, immSel, memWrEn, regWrEn, DwSel, jSel, pcSel, Aa, Ab, Aw, resAluOp, imm, jumpAddr);
 
-	decoder dec(cmdOut, immSel, memWrEn, regWrEn, DwSel, jSel, pcSel, Aa, Ab, Aw, resAluOp, imm, jumpAddr);
 
-
+	// The main logic, which does the "execute" part of fetch / decode / execute
+	
 	mux3 DwMux(Dw, resAluRes, pcAluRes, memOut, DwSel); // Select which data to write
 	regfile rf(Da, Db, Dw, Aa, Ab, Aw, regWrEn, clk); // The register file
 
 	signExt immExter(imm, immExt); // Sign-extend the immediate
 	mux2 DbMux(DbOrImm, Db, immExt, immSel); // Select either immediate or register for the main ALU's second input
-	alu resAlu(resAluRes, , zeroFlag, , Da, DbOrImm, resAluOp); // The main ALU (used for math, data memory ops)
+
+	// The main ALU, used for math and jumping
+	alu resAlu(resAluRes, , zeroFlag, , Da, DbOrImm, resAluOp); 
 
 	// Data memory has two read ports, for instruction and data.  The data port also does writes.
-	dataMemory dm(clk, memWrEn, /*memAddr[9:0]*/ resAluRes[9:0], pc[11:2] /* divide by 4 */, Db, memOut, cmdOut);
+	// Divide PC by 4 so it gets that word, not that byte
+	dataMemory dm(clk, memWrEn, resAluRes[9:0], pc[11:2], Db, memOut, cmd);
 
-	// Adds 4 to the branch location
+	// Adds 4 to the branch location, and multiplies it by 4
 	alu branchAlu(branchAluRes, , , , 32'd4, {14'b0, imm, 2'b0}, 3'd0 /*add command*/);
 
-	/*
-	// Add branchAluRes instead of 4 iff it's a beq / bne, and the zero flag is appropriate
-	mux2 pcAddMux(pcAdd, 32'd4, branchAluRes, pcSel[0] && (pcSel[1] ^ zeroFlag));
-
-	// Add 4 (or some other value) to the pc
-	alu pcAlu(pcAluRes, , , , pc, pcAdd, 3'd0);
-	*/
-
-  // assign pcAluRes = pc + ((pcSel[0] && (pcSel[1] ^ zeroFlag)) ? branchAluRes : 32'd4);
+	// What the PC will be if it doesn't branch
 	assign pcAluRes = pc + ( (pcSel == 2'd1 && zeroFlag == 1) || (pcSel == 2'd2 && zeroFlag == 0) ? branchAluRes : 32'd4);
 
 	// Iff jump instruction, then reset the pc; otherwise add some amount
 	mux3 pcMux(nextPc, Da, {pc[31:28], jumpAddr, 2'b0}, pcAluRes, jSel);
-
-
 
 endmodule
